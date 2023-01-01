a299d028783afb2863d33a7678507884006612fd
kaitona <kaito146remi@gmail.com>
2023-01-01 19:48:40 +0900

    new solution

--- various missions/makingTriangle.py
+++ various missions/makingTriangle.py
@@ -1,17 +1,14 @@
 import sys
 
 def solve(pollCount, polls):
-    hashmap = {}
-    for poll in polls:
-        if poll not in hashmap: hashmap[poll] = 1
-    polls = list(hashmap.keys())
+    result = 0
     polls.sort(reverse = True)
     for i in range(len(polls)-2):
         for j in range(i+1, len(polls)-1):
-            pass
-
-    
-
+            for k in range(j+1, len(polls)):
+                if polls[j] + polls[k] > polls[i] and polls[i] != polls[j] and polls[j] != polls[k]: result += 1
+    print(result)
+            
 data = sys.stdin.readlines()
 pollCount = int(data[0].replace("\n", ""))
 polls = list(map(int, data[1].split(" ")))
