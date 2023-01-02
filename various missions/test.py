35fe15ee98485da733b36b3b6f2d585161a529a1
kaitona <kaito146remi@gmail.com>
2023-01-02 23:03:59 +0900

    change solution

--- various missions/test.py
+++ various missions/test.py
@@ -1,3 +1,18 @@
-s = "1234"
-print(s[0:2])
-print(s[2:4])
+def solve(num):
+    result = 0
+    combinations = []
+    for i in range(2 ** (len(num)-1)):
+        combi=""
+        for j in range(len(num)-1):
+            if((i >> j)&1): combi += num[j] + "+"
+            else: combi += num[j] + ""
+        combi += num[len(num)-1]
+        combinations.append(combi)
+    
+    for combi in combinations:
+        result += eval(combi)
+    print(result)
+
+
+num = list(input())
+solve(num)
