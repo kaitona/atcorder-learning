35e9bd26f333cbd96251694300e82abe8cc5f3d2
kaitona <kaito146remi@gmail.com>
2023-01-05 14:05:46 +0900

    new solution

--- various missions/collatzProblem.py
+++ various missions/collatzProblem.py
@@ -1,14 +1,15 @@
 def solve(num, numList):
-    print(solveHelper(num, numList))
+    return solveHelper(num, numList)
 
 def solveHelper(num, numList):
     for i in range(len(numList)):
         if num == numList[i]:
-            print(len(numList)+1)  
+            print(len(numList)+1)
+            return True
     numList.append(num)
 
     if num % 2 ==0: solveHelper(num/2, numList)
-    if num % 2 ==1: solveHelper(3*num+1, numList)
+    else: solveHelper(3*num+1, numList)
 
 
 num = int(input())
